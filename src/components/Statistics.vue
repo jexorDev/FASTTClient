ba23ce563ae12e4a4aa7edc9a59238ee87651024
Dustin Seger <3589164+jexorDev@users.noreply.github.com>
2024-07-06 12:09:30 -0500

    pax volume statistics

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -2,10 +2,13 @@
     import {ref} from "vue";
 import AirlineAircraft from "../models/AirlineAircraft";
 import axios from "axios";
+import PaxVolumeHour from "../models/PaxVolumeHour";
+import { getDateTimeFromString } from "../utility/DateTimeUtility";
 
     const airlineAircrafts = ref<AirlineAircraft[]>([]);
     const loading = ref(false);
     const airlineAircraftsLoaded = ref(false);
+    const paxVolumePerHour = ref<PaxVolumeHour[]>([]);
 
     async function loadAirlineAircrafts() {
         axios.get(`${import.meta.env.VITE_API_BASE_URL}/AirlineAircrafts`).then((response) => {        
@@ -23,13 +26,31 @@ import axios from "axios";
     }
 
     async function loadPaxVolume() {
-        
+        axios.get(`${import.meta.env.VITE_API_BASE_URL}/Statistics?fromDateTime=${getDateTimeFromString("00:00").toISOString()}&toDateTime=${getDateTimeFromString("11:59").toISOString()}`).then((response) => {        
+            paxVolumePerHour.value = response.data as PaxVolumeHour[];      
+        }).finally(() => loading.value = false);
     }
 
 </script>
 <template>
     <h3>PAX VOLUME</h3>    
     <a @click="loadPaxVolume()">[LOAD]</a>
+    <table>
+        <tr>
+            <th>HOUR</th>
+            <th>ARRIVING PAX</th>
+            <th>ARRIVING FLIGHTS</th>
+            <th>DEPARTING PAX</th>
+            <th>DEPARTING FLIGHTS</th>
+        </tr>
+        <tr v-for="row in paxVolumePerHour">
+            <td>{{ row.hour }}</td>
+            <td>{{ row.arrivingPassengers }}</td>
+            <td>{{ row.arrivingFlights }}</td>
+            <td>{{ row.departingPassengers }}</td>
+            <td>{{ row.departingFlights }}</td>
+        </tr>
+    </table>
     <h3>AIRLINE AIRCRAFTS</h3>    
     <a @click="loadAirlineAircrafts()">[LOAD]</a>
     <table>
